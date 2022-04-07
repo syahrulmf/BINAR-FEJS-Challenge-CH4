d4d3a557513c7d49ca27e7dad96c685e977daaca
syahrulmf <50761419+syahrulmf@users.noreply.github.com>
2022-04-07 23:01:36 +0700

    add detail page

--- src/routes/MainRoute.jsx
+++ src/routes/MainRoute.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import FormSearch from '../components/Form/FormSearch';
+import Detail from '../pages/Detail/Detail';
 import Search from '../pages/Search/Search';
 
 export default function MainRoute() {
@@ -9,7 +10,7 @@ export default function MainRoute() {
       <Routes>
         <Route path='/' element={<FormSearch />} />
         <Route path='/search' element={<Search />} />
-        <Route path='/detail/:id' element={<Search />} />
+        <Route path='/detail' element={<Detail />} />
       </Routes>
     </>
   )
